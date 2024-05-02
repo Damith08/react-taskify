ce9b87b0e4a8ce9afdcb8e2cbc1758aefca01861
Damith08 <dmthsamarasinghe@gmail.com>
2024-05-03 01:43:06 +0530

    feat: single todo component
    create and style SingleTodo Component

--- src/components/SingleTodo.tsx
+++ src/components/SingleTodo.tsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import { Todo } from '../model'
+import { AiFillDelete, AiFillEdit } from "react-icons/ai"
+import { MdDone } from "react-icons/md";
+import './styles.css';
 
 type Props = {
     todo: Todo,
@@ -9,7 +12,22 @@ type Props = {
 
 const SingleTodo = ({ todo, todos, setTodos }: Props) => {
     return (
-        <div>SingleTodo</div>
+        <form className='todos__single'>
+            <span className="todos__single--text">
+                {todo.todo}
+            </span>
+            <div>
+                <span className="icon">
+                    <AiFillEdit />
+                </span>
+                <span className="icon">
+                    <AiFillDelete />
+                </span>
+                <span className="icon">
+                    <MdDone />
+                </span>
+            </div>
+        </form>
     )
 }
 
